c3c968d5b83b985c51152f4720697483d3a10be1
Marcy <marcy9114@gmail.com>
2014-06-26 13:56:10 +0900

    fix ps1 file template syntax error

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'marcy9114@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures powershell_cloudwatch'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.7'
+version          '0.1.8'
 depends          'windows_scheduler'
