adc61dcc1ea72a4e967a6941e8d7a033ff527bb7
Marcy <marcy9114@gmail.com>
2014-06-26 09:58:21 +0900

    add install AWSTools for PowerShell recipe

---
+++ recipes/default.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: powershell_cloudwatch
+# Recipe:: default
+#
+# Copyright 2014, Masashi Terui
+#
+# Apache 2.0 License
+#
+
+remote_file "C:\\AWSToolsAndSDKForNet.msi" do
+  source "http://sdk-for-net.amazonwebservices.com/latest/AWSToolsAndSDKForNet.msi"
+end
+
+powershell_script "import-module AWSPowerShell" do
+  cwd Chef::Config[:file_cache_path]
+  code <<-EOH
+  import-module "C:\\Program Files (x86)\\AWS Tools\\PowerShell\\AWSPowerShell\\ßAWSPowerShell.psd1"
+  EOH
+  action :nothing
+end
+
+windows_package "AWS Toolkit for PowerShell" do
+  action :install
+  source "C:\\AWSToolsAndSDKForNet.msi"
+  notifies :run, "powershell_script[import-module AWSPowerShell]"
+end
